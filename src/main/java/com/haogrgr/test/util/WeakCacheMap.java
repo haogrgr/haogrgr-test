a5e3c0792c8331c3c880fc8e43f132321a04831a
haogrgr <haogrgr@126.com>
2014-07-03 16:51:18 +0800

    WeakCacheMap add commit

--- src/main/java/com/haogrgr/test/util/WeakCacheMap.java
+++ src/main/java/com/haogrgr/test/util/WeakCacheMap.java
@@ -5,29 +5,37 @@ import java.lang.ref.WeakReference;
 import java.util.Map;
 import java.util.WeakHashMap;
 
+/**
+ * 弱引用Map,适合做缓存,包装WeakHashMap, 将Value也使用WeakReference,
+ * 防止当WeakHashMap的key回收后,还是有value的强引用,
+ * 需要等到下一次WeakHashMap相关方法调用才会清除key为空的value
+ * @see java.util.WeakHashMap.expungeStaleEntries()
+ * <p>Author: desheng.tu</p>
+ * <p>Date: 2014年7月3日</p>
+ */
 public class WeakCacheMap<K, V> {
 
     private final Map<K, Reference<V>> map = new WeakHashMap<K, Reference<V>>();
 
-    public V get(K paramK) {
-        Reference<V> localReference = this.map.get(paramK);
+    public V get(K key) {
+        Reference<V> localReference = this.map.get(key);
         if (localReference == null) {
             return null;
         }
         
         V localObject = localReference.get();
         if (localObject == null) {
-            this.map.remove(paramK);
+            this.map.remove(key);
         }
         
         return localObject;
     }
 
-    public void put(K paramK, V paramV) {
-        if (paramV != null) {
-            this.map.put(paramK, new WeakReference<V>(paramV));
+    public void put(K key, V value) {
+        if (value != null) {
+            this.map.put(key, new WeakReference<V>(value));
         } else
-            this.map.remove(paramK);
+            this.map.remove(key);
     }
 
     public void clear() {
