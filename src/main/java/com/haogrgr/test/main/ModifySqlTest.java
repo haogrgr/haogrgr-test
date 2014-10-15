b414f677649478d20c5a7156def11deb508cd4d1
haogrgr <haogrgr@126.com>
2014-10-15 17:18:17 +0800

    例子改进,使用框架提供的工具类.

--- src/main/java/com/haogrgr/test/main/ModifySqlTest.java
+++ src/main/java/com/haogrgr/test/main/ModifySqlTest.java
@@ -1,33 +1,28 @@
 package com.haogrgr.test.main;
 
+import com.alibaba.druid.sql.SQLUtils;
 import com.alibaba.druid.sql.ast.SQLExpr;
 import com.alibaba.druid.sql.ast.expr.SQLBinaryOpExpr;
 import com.alibaba.druid.sql.ast.expr.SQLBinaryOperator;
 import com.alibaba.druid.sql.ast.statement.SQLSelectStatement;
 import com.alibaba.druid.sql.dialect.mysql.ast.statement.MySqlSelectQueryBlock;
-import com.alibaba.druid.sql.parser.SQLExprParser;
-import com.alibaba.druid.sql.parser.SQLParserUtils;
-import com.alibaba.druid.sql.parser.SQLStatementParser;
 import com.alibaba.druid.util.JdbcUtils;
 
 public class ModifySqlTest {
 
 	public static void main(String[] args) {
-		SQLStatementParser statementParser = SQLParserUtils.createSQLStatementParser("select * from dual where 1=1", JdbcUtils.MYSQL);
-
-		SQLSelectStatement select = statementParser.parseSelect();
-
-		MySqlSelectQueryBlock query = (MySqlSelectQueryBlock) select.getSelect().getQuery();
+		SQLSelectStatement statement = (SQLSelectStatement) SQLUtils.parseStatements("select * from dual where 1=1", JdbcUtils.MYSQL).get(0);
+		MySqlSelectQueryBlock query = (MySqlSelectQueryBlock) statement.getSelect().getQuery();
 
 		SQLExpr where = query.getWhere();
-		System.out.println(select.toString());
+		System.out.println(SQLUtils.toSQLString(statement));
 
-		SQLExprParser exprParser = SQLParserUtils.createExprParser("a = ?", JdbcUtils.MYSQL);
-		SQLExpr nwhere = new SQLBinaryOpExpr(where, SQLBinaryOperator.BooleanAnd, exprParser.expr());
+		SQLExpr append = SQLUtils.toSQLExpr("a = ? and b = ?", JdbcUtils.MYSQL);
+		where = new SQLBinaryOpExpr(where, SQLBinaryOperator.BooleanAnd, append);
 
-		query.setWhere(nwhere);
+		query.setWhere(where);
 
-		System.err.println(select.toString());
+		System.err.println(SQLUtils.toSQLString(statement));
 	}
 
 }
