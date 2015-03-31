d1bc6dfb604d391e9ffad15e8c82051dfc433101
haogrgr <haogrgr@126.com>
2015-03-31 16:53:07 +0800

    Spring的AOP相关工具类,以及BeanWrapper的了解.

---
+++ src/main/java/com/haogrgr/test/util/SpringAopUtils.java
@@ -0,0 +1,86 @@
+package com.haogrgr.test.util;
+
+import java.lang.reflect.Method;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.DynamicMethodMatcherPointcut;
+
+public class SpringAopUtils {
+
+	public static void main(String[] args) {
+		//需要被代理的类
+		TobeProxy bean = new TobeProxy();
+		
+		//组合Pointcut(拦截点)和Advisor(附加逻辑)
+		DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(new SimpleDynamicPointcut(), new SimpleAdvice());
+		
+		//代理工厂
+		ProxyFactory factory = new ProxyFactory();
+		factory.addAdvisor(advisor);
+		factory.setTarget(bean);
+		
+		TobeProxy proxy = (TobeProxy)factory.getProxy();
+		proxy.foo(1);
+		proxy.foo(100);//符合拦截条件,被拦截
+		proxy.bar();
+		
+	}
+
+	public static class TobeProxy {
+
+		public void foo(int x) {
+			System.out.println("foo : " + x);
+		}
+
+		public void bar() {
+			System.out.println("bar");
+		}
+
+	}
+
+	public static class SimpleAdvice implements MethodInterceptor {
+
+		@Override
+		public Object invoke(MethodInvocation invocation) throws Throwable {
+			System.out.println("before invocation ... ");
+			Object result = invocation.proceed();
+			System.out.println("after invocation ... ");
+			return result;
+		}
+
+	}
+
+	/**
+	 * 动态的Pointcut, 比较费性能, 因为每次调用都要基于参数值判断是否应用Advice, 无法缓存
+	 * 
+	 * 对于的, 有静态的Pointcut, StaticMethodMatcherPointcut类
+	 *
+	 * @date 2015年3月31日 下午4:42:40
+	 * @author desheng.tu
+	 */
+	public static class SimpleDynamicPointcut extends DynamicMethodMatcherPointcut {
+
+		@Override
+		public boolean matches(Method method, Class<?> targetClass, Object[] args) {
+			if ("foo".equals(method.getName()) && ((int) args[0]) == 100) {
+				return true;
+			}
+			return false;
+		}
+
+		@Override
+		public ClassFilter getClassFilter() {
+			return new ClassFilter() {
+				@Override
+				public boolean matches(Class<?> clazz) {
+					return clazz.isAssignableFrom(TobeProxy.class);
+				}
+			};
+		}
+
+	}
+}
---
+++ src/main/java/com/haogrgr/test/util/SpringBeanWrapper.java
@@ -0,0 +1,67 @@
+package com.haogrgr.test.util;
+
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.DirectFieldAccessor;
+import org.springframework.beans.PropertyAccessorFactory;
+
+/**
+ * Spring的BeanWrapper可以方便的设置属性,对象图,PropertyEditor支持
+ *
+ * @date 2015年3月31日 下午4:31:52
+ * @author desheng.tu
+ */
+public class SpringBeanWrapper {
+
+	public static void main(String[] args) {
+		Department department = new Department();
+		department.setName("test");
+		Department parent = new Department();
+		parent.setName("parent");
+		department.setParent(parent);
+		
+		BeanWrapper wrapper = PropertyAccessorFactory.forBeanPropertyAccess(department);
+		wrapper.setPropertyValue("name", "test1");
+		wrapper.setPropertyValue("parent.name", "parent1");
+		
+		System.out.println(department.getName());
+		System.out.println(department.getParent().getName());
+		
+		//========================================
+		
+		Department dept = new Department();
+		
+		DirectFieldAccessor accessor = new DirectFieldAccessor(dept);
+		accessor.setPropertyValue("level", 5);
+		
+		System.out.println(dept.getLevel());
+	}
+
+	public static class Department {
+
+		private Department parent;
+		private String name;
+		private int level;
+
+		public Department getParent() {
+			return parent;
+		}
+
+		public void setParent(Department parent) {
+			this.parent = parent;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public int getLevel() {
+			return level;
+		}
+
+	}
+
+}
