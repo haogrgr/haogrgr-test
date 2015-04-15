60e230ee4bf00383978897dd0af25aea08b0c305
desheng.tu <haogrgr@126.com>
2015-04-15 12:53:20 +0800

    test表建表语句

---
+++ src/main/resources/test.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `test` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `modify_time` datetime DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
