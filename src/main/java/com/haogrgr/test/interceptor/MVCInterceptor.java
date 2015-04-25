766a2a9b91f353a8c2e0f6de57223cef37b0cd7b
haogrgr <haogrgr@126.com>
2015-04-25 14:44:53 +0800

    HandlerInterceptorAdapter源码debug

--- src/main/java/com/haogrgr/test/interceptor/MVCInterceptor.java
+++ src/main/java/com/haogrgr/test/interceptor/MVCInterceptor.java
@@ -3,6 +3,7 @@ package com.haogrgr.test.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class MVCInterceptor extends HandlerInterceptorAdapter {
@@ -11,5 +12,11 @@ public class MVCInterceptor extends HandlerInterceptorAdapter {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         return super.preHandle(request, response, handler);
     }
-
+    
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+    		ModelAndView modelAndView) throws Exception {
+    	super.postHandle(request, response, handler, modelAndView);
+    }
+    
 }
