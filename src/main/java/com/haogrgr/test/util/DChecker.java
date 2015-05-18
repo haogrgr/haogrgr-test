9b7ed95913e13dfaa6278b343171dc74853a5196
desheng.tu <haogrgr@126.com>
2015-05-18 15:34:41 +0800

    data checker

---
+++ src/main/java/com/haogrgr/test/util/DChecker.java
@@ -0,0 +1,20 @@
+package com.haogrgr.test.util;
+
+/**
+ * data checker 数据格式校验工具类
+ * 
+ * @author desheng.tu
+ * @date 2015年5月18日 下午3:17:37
+ *
+ */
+public class DChecker {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static final boolean isEmail(CharSequence value) {
+		return false;
+	}
+
+}
