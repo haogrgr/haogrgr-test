c113edf3049265b108d7b385e192413ec1eddb18
desheng.tu <haogrgr@126.com>
2015-07-24 16:21:24 +0800

    1.改为静态方法, 去掉没啥用的(想多了~~~简单就好).

--- src/main/java/com/haogrgr/test/util/SystemTimer.java
+++ src/main/java/com/haogrgr/test/util/SystemTimer.java
@@ -2,17 +2,14 @@ package com.haogrgr.test.util;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 不精确的时间调用. System.currentTimeMillis()调用是系统调用, 虽然不慢, 但是频繁调用损耗还是比较大的. 当不需要精确的时间时,
  * 可以使用定时任务来缓存时间, 通过牺牲精度来换取性能.
  * 
  * @see https://blogs.oracle.com/dholmes/entry/inside_the_hotspot_vm_clocks
- * @see http://blog.csdn.net/yang_net/article/details/6056254
+ * @see http://www.blogjava.net/killme2008/archive/2014/09/01/338420.html
  * 
  * @author desheng.tu
  * @date 2015年7月23日 下午7:41:03
@@ -20,78 +17,30 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class SystemTimer {
 
-	private static ScheduledExecutorService scheduler = Executors
-			.newSingleThreadScheduledExecutor(new DefaultThreadFactory());
+	private static ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor((r) -> {
+		return new Thread(r, "systime-schedule-thread");
+	});
 
-	private volatile long now = System.currentTimeMillis();
-	private volatile ScheduledFuture<?> task;
+	private static final long tickUnit = Long.parseLong(System.getProperty("notify.systimer.tick", "50"));
 
-	/**
-	 * 创建系统时间
-	 * @param tick 时间更新周期
-	 * @param unit 时间更新周期单位
-	 */
-	public SystemTimer(int tick, TimeUnit unit) {
-		if (tick <= 0) {
-			throw new IllegalArgumentException("tick 不能小于零  : " + tick);
-		}
-
-		task = scheduler.scheduleAtFixedRate(() -> {
-			now = System.currentTimeMillis();
-		}, 0, tick, unit);
-
-		try {
-			Thread.sleep(unit.toMillis(tick));
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+	private static volatile long now = System.currentTimeMillis();
 
 	/**
 	 * 获取不准确的当前时间
 	 */
-	public long now() {
+	public long currentTimeMillis() {
 		return now;
 	}
 
-	/**
-	 * 修改系统时间更新频率
-	 * @param tick 时间更新周期
-	 * @param unit 时间更新周期单位
-	 */
-	public void update(int tick, TimeUnit unit) {
-		if (tick <= 0) {
-			throw new IllegalArgumentException("tick 不能小于零  : " + tick);
-		}
+	static {
+		executor.scheduleAtFixedRate(() -> now = System.currentTimeMillis(), tickUnit, tickUnit, TimeUnit.MILLISECONDS);
 
-		ScheduledFuture<?> tasknew = scheduler.scheduleAtFixedRate(() -> {
-			now = System.currentTimeMillis();
-		}, 0, tick, unit);
-
-		task.cancel(true);
-
-		task = tasknew;
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				executor.shutdown();
+			}
+		});
 	}
 
-	/**
-	 * 停止线程
-	 */
-	public static void stop() {
-		scheduler.shutdown();
-
-		try {
-			scheduler.awaitTermination(10, TimeUnit.SECONDS);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-
-	static class DefaultThreadFactory implements ThreadFactory {
-
-		private static final AtomicInteger adder = new AtomicInteger(1);
-
-		public Thread newThread(Runnable r) {
-			return new Thread(r, "systime-schedule-thread-" + adder.getAndIncrement());
-		}
-	}
 }
