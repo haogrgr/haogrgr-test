de6cb0112c9a78a8a454b7aea71aa90a9f4182c2
tudesheng <tudesheng@didichuxing.com>
2015-10-22 16:52:41 +0800

    1.代码结构调整.

--- src/main/java/com/haogrgr/test/main/SpringBeanDefFindTest.java
+++ src/main/java/com/haogrgr/test/main/SpringBeanDefFindTest.java
@@ -1,4 +1,4 @@
-package com.haogrgr.test.util;
+package com.haogrgr.test.main;
 
 import java.io.IOException;
 import java.util.Set;
@@ -9,19 +9,17 @@ import org.springframework.core.type.classreading.MetadataReader;
 import org.springframework.core.type.classreading.MetadataReaderFactory;
 import org.springframework.core.type.filter.TypeFilter;
 
-import com.haogrgr.test.anno.Test;
-
 /**
  * 使用Spring的类扫描并获取bean定义.
  */
-@Test("test")
-public class SpringBeanDefFindUtil {
+@SpringBeanDefFindTestAnno("test")
+public class SpringBeanDefFindTest {
 
 	public static void main(String[] args) {
 		ClassPathScanningCandidateComponentProvider provider = new ClassPathScanningCandidateComponentProvider(false);
 		provider.addIncludeFilter(new TestTypeFilter());
 
-		Set<BeanDefinition> beans = provider.findCandidateComponents("com.haogrgr.test.util");
+		Set<BeanDefinition> beans = provider.findCandidateComponents("com.haogrgr.test.main");
 
 		for (BeanDefinition bdf : beans) {
 			System.out.println(bdf.getBeanClassName());
@@ -31,8 +29,9 @@ public class SpringBeanDefFindUtil {
 	public static final class TestTypeFilter implements TypeFilter {
 		@Override
 		public boolean match(MetadataReader metadataReader, MetadataReaderFactory metadataReaderFactory) throws IOException {
-			return metadataReader.getAnnotationMetadata().isAnnotated("com.haogrgr.test.anno.Test");
+			return metadataReader.getAnnotationMetadata().isAnnotated("com.haogrgr.test.main.SpringBeanDefFindTestAnno");
 		}
 	}
 	
 }
+
---
+++ src/main/java/com/haogrgr/test/main/SpringBeanDefFindTestAnno.java
@@ -0,0 +1,12 @@
+package com.haogrgr.test.main;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface SpringBeanDefFindTestAnno {
+	String value();
+}
