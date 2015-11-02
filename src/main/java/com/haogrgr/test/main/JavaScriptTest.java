c17aa23ca2f1ba6e0aa687afd5888cd0371712eb
tudesheng <tudesheng@didichuxing.com>
2015-11-02 11:13:44 +0800

    1.Java调用js, js中引用Java类型.

--- src/main/java/com/haogrgr/test/main/JavaScriptTest.java
+++ src/main/java/com/haogrgr/test/main/JavaScriptTest.java
@@ -1,8 +1,17 @@
 package com.haogrgr.test.main;
 
+import java.math.BigDecimal;
+
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 
+/**
+ * Java调用js
+ * 
+ * @author desheng.tu
+ * @date 2015年11月2日 上午11:12:17
+ *
+ */
 public class JavaScriptTest {
 
 	public static void main(String[] args) throws Exception {
@@ -10,9 +19,11 @@ public class JavaScriptTest {
 		ScriptEngine engine = manager.getEngineByName("JavaScript");
 
 		User user = new User(1, "haogrgr");
+		user.setMoney(BigDecimal.TEN);
 		engine.put("user", user);
 
-		engine.eval("if(user.id == 1) {user.name='xxxxx'; user.id = 2;}");
+		engine.eval("var BigDecimal = Java.type('java.math.BigDecimal');" + "if(user.id == 1) {" + "user.name='xxxxx';"
+				+ "user.id = 2;" + "user.money = new BigDecimal(user.id.toString()).multiply(new BigDecimal(4));" + "}");
 
 		System.out.println(user);
 	}
@@ -20,6 +31,7 @@ public class JavaScriptTest {
 	public static class User {
 		private Integer id;
 		private String name;
+		private BigDecimal money;
 
 		public User(Integer id, String name) {
 			super();
@@ -43,9 +55,17 @@ public class JavaScriptTest {
 			this.name = name;
 		}
 
+		public BigDecimal getMoney() {
+			return money;
+		}
+
+		public void setMoney(BigDecimal money) {
+			this.money = money;
+		}
+
 		@Override
 		public String toString() {
-			return "User [id=" + id + ", name=" + name + "]";
+			return "User [id=" + id + ", name=" + name + ", money=" + money + "]";
 		}
 
 	}
