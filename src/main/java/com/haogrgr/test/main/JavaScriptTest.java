4f5a3b33544b97d3ba4b378bdd86aee449dd33cc
tudesheng <tudesheng@didichuxing.com>
2015-11-16 14:57:26 +0800

    1.spring审计到4.2.3. 2.生成验证码的工具类, 网上找的. 3.Java调用js例子, java7和java8默认的引擎引入Java类的方式不一样.

--- src/main/java/com/haogrgr/test/main/JavaScriptTest.java
+++ src/main/java/com/haogrgr/test/main/JavaScriptTest.java
@@ -20,10 +20,14 @@ public class JavaScriptTest {
 
 		User user = new User(1, "haogrgr");
 		user.setMoney(BigDecimal.TEN);
+		user.setAge(10);
 		engine.put("user", user);
 
-		engine.eval("var BigDecimal = Java.type('java.math.BigDecimal');" + "if(user.id == 1) {" + "user.name='xxxxx';"
-				+ "user.id = 2;" + "user.money = new BigDecimal(user.id.toString()).multiply(new BigDecimal(4));" + "}");
+		//注意: java7 下,通过   importClass(java.math.BigDecimal) 来引用Java类
+		//		String imports = "importClass(java.math.BigDecimal);";//java7;
+		String imports = "var BigDecimal = Java.type('java.math.BigDecimal');";//java8;
+		engine.eval(imports + "if(user.id == 1) {" + "user.name='xxxxx';" + "user.id = 2;"
+				+ "user.money = new BigDecimal(user.age / 4);" + "}");
 
 		System.out.println(user);
 	}
@@ -31,6 +35,7 @@ public class JavaScriptTest {
 	public static class User {
 		private Integer id;
 		private String name;
+		private Integer age;
 		private BigDecimal money;
 
 		public User(Integer id, String name) {
@@ -53,6 +58,14 @@ public class JavaScriptTest {
 
 		public void setName(String name) {
 			this.name = name;
+		}
+
+		public Integer getAge() {
+			return age;
+		}
+
+		public void setAge(Integer age) {
+			this.age = age;
 		}
 
 		public BigDecimal getMoney() {
