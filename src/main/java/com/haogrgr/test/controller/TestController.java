dda93a6204550f6215f3b30684ea3033ce1f4645
tudesheng <tudesheng@didichuxing.com>
2015-11-25 19:09:26 +0800

    1.pom依赖换为最新的版本. 2.Spring继承Bean Validation的方式瞄瞄.

--- src/main/java/com/haogrgr/test/controller/TestController.java
+++ src/main/java/com/haogrgr/test/controller/TestController.java
@@ -1,8 +1,12 @@
 package com.haogrgr.test.controller;
 
 import javax.annotation.Resource;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,17 +27,37 @@ public class TestController {
 		testService.testExp();
 		return null;
 	}
-	
+
 	@RequestMapping("/json")
 	public @ResponseBody Object jsontest(@RequestBody PageInfo<Test> pageInfo) {
 		System.out.println(pageInfo.getTotal());
 		return pageInfo;
 	}
-	
+
 	@RequestMapping("/json2")
 	public @ResponseBody Object jsontest2(PageInfo<Test> pageInfo) {
 		System.out.println(pageInfo.getTotal());
 		return pageInfo;
+	}
+
+	@RequestMapping("/valid")
+	public @ResponseBody Object valid(@Valid Vlid v, BindingResult result, Model model) {
+		System.out.println(v);
+		return v;
+	}
+
+	public static class Vlid {
+		@NotNull
+		private String name;
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
 	}
 
 }
