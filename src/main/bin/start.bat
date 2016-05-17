e28e616a3aa0738688d16a9336772c6a9fb615f3
tudesheng <tudesheng@didichuxing.com>
2016-05-17 15:36:49 +0800

    备份

--- src/main/bin/start.bat
+++ src/main/bin/start.bat
@@ -1,7 +1,17 @@
 @echo off
-
 SetLocal EnableDelayedExpansion
 
-java -java .\lib\${project.build.finalName}.jar
+SET MAIN_CLASS=${main.class}
+
+SET JAVA_OPTS=-Xms2048m -Xmx2048m -XX:NewSize=1024m -XX:PermSize=512m -server -XX:+DisableExplicitGC -verbose:gc -XX:+PrintGCDateStamps -XX:+PrintGCDetails
+
+SET JAVA_PROPS=-D${project.parent.groupId}.${project.parent.artifactId}
+
+SET CLASSPATH=.;.\conf
+FOR %%i IN ("./lib/*.jar") DO SET CLASSPATH=!CLASSPATH!;.\lib\%%i
+
+ECHO java %JAVA_OPTS% %JAVA_PROPS% -cp %CLASSPATH% %MAIN_CLASS%
+
+java %JAVA_OPTS% %JAVA_PROPS% -cp %CLASSPATH% %MAIN_CLASS%
 
 EndLocal
