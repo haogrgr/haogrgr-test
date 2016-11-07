f2ec7e7b67744c4599e87fda7d889a7a68633f83
haogrgr <haogrgr1@126.com>
2016-11-07 10:53:24 +0800

    例子

--- src/test/java/com/haogrgr/test/main/Temp.java
+++ src/test/java/com/haogrgr/test/main/Temp.java
@@ -1,11 +1,26 @@
 package com.haogrgr.test.main;
 
+import java.util.ArrayList;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.haogrgr.test.dao.TestMapper;
+import com.haogrgr.test.model.TestModel;
+import com.haogrgr.test.util.Lists;
 
 public class Temp {
 
 	public static void main(String[] args) throws Exception {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring-root.xml");
+		TestMapper bean = context.getBean(TestMapper.class);
+
+		ArrayList<TestModel> array = Lists.array(new TestModel().setName("hehe"), new TestModel().setName("hehe"));
+		bean.saveBatch(array);
+
+		for (TestModel testModel : array) {
+			System.out.println(testModel.getId());
+		}
+
 		context.close();
 	}
 
